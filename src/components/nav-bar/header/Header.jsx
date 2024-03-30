59c944875a47a6dedeb95351c9ee7bbeaaabc4c7
Jamisson Lira Ramos <missonlira@gmail.com>
2024-03-30 12:29:40 -0300

    Criando component de sub menu do avatar

--- src/components/nav-bar/header/Header.jsx
+++ src/components/nav-bar/header/Header.jsx
@@ -7,7 +7,7 @@
 
     //Material UI
     // Button, Tab, Tabs, Toolbar, Typography, useMediaQuery, useTheme,
-    import { AppBar, Avatar, Box, Divider, ListItemIcon, Menu, MenuItem, Tab, Tabs, Toolbar, Tooltip, Typography, useMediaQuery, useTheme } from "@mui/material";
+    import { AppBar, Box, Tab, Tabs, Toolbar, Typography, useMediaQuery, useTheme } from "@mui/material";
 
     //Icons
     import GavelIcon from '@mui/icons-material/Gavel';
@@ -15,28 +15,20 @@
     import LanguageIcon from '@mui/icons-material/Language';
     import MedicalServicesIcon from '@mui/icons-material/MedicalServices';
     import InfoIcon from '@mui/icons-material/Info';
-    import AddCircleIcon from '@mui/icons-material/AddCircle';
-    import DashboardCustomizeIcon from '@mui/icons-material/DashboardCustomize';
-    import LogoutIcon from '@mui/icons-material/Logout';
 
-    //Imagens
-    import perfilImage from '../../../assets/perfil.jpg'; // Importe a imagem usando o caminho relativo correto
-    
     //CSS
     import styles from './Header.module.css'
 
     //Components
     import DrawerComp from "../drawer-comp/DrawerComp";
+    import SubMenu from "../sub-menu/SubMenu";
 
     const Header = () => {
 
         const [value, setValue] = useState(0); //Monitorando os estados do btns do menu;
-        const [anchorEl, setAnchorEl] = useState(null); //Monitorar o menu do avatar;
         const theme = useTheme();
 
-        console.log(theme)
         const isMatch = useMediaQuery(theme.breakpoints.down('md')) //Retorna true de a tela for igual a medida definida
-        console.log(isMatch);
 
         const pages = [
             {id: 1, page: 'HOME', icon: <HomeIcon/>, linkPage: '/'},
@@ -44,22 +36,6 @@
             {id: 3, page: 'SERVIÇOS', icon: <MedicalServicesIcon/>, linkPage: '/'},
             {id: 4, page: 'SOBRE', icon: <InfoIcon/>, linkPage: '/about'},
         ];
-            
-        const pagesSubMenu = [
-            {id: 1, page: 'NOVO POST', icon: <AddCircleIcon/>, linkPage: '/'},
-            {id: 2, page: 'DASHBOARD', icon: <DashboardCustomizeIcon/>, linkPage: '/'},
-            {id: 3, page: 'SAIR', icon: <LogoutIcon/>, linkPage: '/'}
-        ];
-        
-        //Clique do btns do avatar, retorna true 
-        const handleClick = (e) => {
-            setAnchorEl(e.currentTarget);
-        };
-
-        //Fechar o menu do avatar
-        const handleClose = () => {
-            setAnchorEl(null);
-        };
         
         return (
 
@@ -118,70 +94,7 @@
                         )
                     }
 
-                    {/* Coloca o nome do usuário logado no sistema  */}
-                    <Tooltip title="Alana"> 
-
-                        <Avatar alt="Remy Sharp" src={perfilImage} onClick={handleClick} />
-
-                    </Tooltip>
-
-                    <Menu
-                        anchorEl={anchorEl}
-                        open={Boolean(anchorEl)}
-                        onClose={handleClose}
-                        sx={{mt: '4px', ml: '-18px' }}
-                        anchorOrigin={{
-                        vertical: 'bottom',
-                        horizontal: 'right',
-                        }}
-                        transformOrigin={{
-                        vertical: 'top',
-                        horizontal: 'right',
-                        }}
-                    >
-                        {
-                            pagesSubMenu.map(({id, page, icon, linkPage}) => (
-
-                            page !== "SAIR" ? 
-
-                                <MenuItem key={id} onClick={handleClose}  component={Link} to={linkPage} > 
-
-                                    <ListItemIcon  sx={{color: 'var(--blue-800)', fontWeight: 'bold'}}>
-                                        {icon} 
-                                    </ListItemIcon>
-
-                                    <Typography 
-                                        textAlign="center" 
-                                        sx={{color: 'var(--blue-800)', fontSize: '.8rem', fontWeight: 'bold'}}>
-                                        {page}
-                                    </Typography>
-
-                                </MenuItem> 
-                                :
-                                <Box key={id} sx={{paddingY: '.8rem'}}>
-
-                                    <Divider/>
-                                    
-                                    <MenuItem key={id} onClick={handleClose}  sx={{paddingY: '.8rem'}} component={Link} to={linkPage}> 
-
-                                        <ListItemIcon>
-                                            <LogoutIcon  sx={{color: 'var(--blue-800)', fontWeight: 'bold'}}/> 
-                                        </ListItemIcon>
-
-                                        <Typography 
-                                            textAlign="center" 
-                                            sx={{color: 'var(--blue-800)', fontSize: '.8rem', fontWeight: 'bold'}}>
-                                            {page}
-                                        </Typography>
-
-                                    </MenuItem>
-
-                                </Box>
-                            ))
-                        }
-
-                    </Menu>
-
+                    <SubMenu/>
                 </AppBar>
 
             </nav>
---
+++ src/components/nav-bar/sub-menu/SubMenu.jsx
@@ -0,0 +1,108 @@
+//React
+import { useState } from "react";
+
+//React Router
+import { Link } from "react-router-dom";
+
+//MAterial UI
+import { Avatar, Box, Divider, ListItemIcon, Menu, MenuItem, Tooltip, Typography } from "@mui/material"
+
+//Icons
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import DashboardCustomizeIcon from '@mui/icons-material/DashboardCustomize';
+import LogoutIcon from '@mui/icons-material/Logout';
+
+//Imagens
+import perfilImage from '../../../assets/perfil.jpg'; // Importe a imagem usando o caminho relativo correto
+
+
+const SubMenu = () => {
+    const [anchorEl, setAnchorEl] = useState(null); //Monitorar o menu do avatar;
+
+    const handleClick = (e) => {
+        setAnchorEl(e.currentTarget);
+    };
+    //Fechar o menu do avatar
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const pagesSubMenu = [
+        {id: 1, page: 'NOVO POST', icon: <AddCircleIcon/>, linkPage: '/'},
+        {id: 2, page: 'DASHBOARD', icon: <DashboardCustomizeIcon/>, linkPage: '/'},
+        {id: 3, page: 'SAIR', icon: <LogoutIcon/>, linkPage: '/'}
+    ];
+
+    return ( 
+
+        <>
+            {/* Coloca o nome do usuário logado no sistema  */}
+            <Tooltip title="Alana"> 
+
+                <Avatar alt="Remy Sharp" src={perfilImage} onClick={handleClick} />
+
+            </Tooltip>
+
+            <Menu
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+                sx={{mt: '4px', ml: '-18px' }}
+                anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'right',
+                }}
+                transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+                }}
+            >
+                {
+                    pagesSubMenu.map(({id, page, icon, linkPage}) => (
+
+                        page !== "SAIR" ? 
+
+                            <MenuItem key={id} onClick={handleClose}  component={Link} to={linkPage} > 
+
+                                <ListItemIcon  sx={{color: 'var(--blue-800)', fontWeight: 'bold'}}>
+
+                                    {icon} 
+
+                                </ListItemIcon>
+
+                                <Typography 
+                                    textAlign="center" 
+                                    sx={{color: 'var(--blue-800)', fontSize: '.8rem', fontWeight: 'bold'}}>
+                                    {page}
+                                </Typography>
+
+                            </MenuItem> 
+                            :
+                            <Box key={id} sx={{paddingY: '.8rem'}}>
+
+                                <Divider/>
+            
+                                <MenuItem key={id} onClick={handleClose}  sx={{paddingY: '.8rem'}} component={Link} to={linkPage}> 
+
+                                    <ListItemIcon>
+                                        <LogoutIcon  sx={{color: 'var(--blue-800)', fontWeight: 'bold'}}/> 
+                                    </ListItemIcon>
+
+                                    <Typography 
+                                        textAlign="center" 
+                                        sx={{color: 'var(--blue-800)', fontSize: '.8rem', fontWeight: 'bold'}}>
+                                        {page}
+                                    </Typography>
+
+                                </MenuItem>
+
+                            </Box>
+                    ))
+                }
+
+            </Menu>
+        </>
+    )
+}
+
+export default SubMenu
